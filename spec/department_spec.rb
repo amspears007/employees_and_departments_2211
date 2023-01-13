d744c2a7beb480ba867c6e85894f1781bfe26782
Amy Spears <amspears007@gmail.com>
2023-01-13 09:45:45 -0700

    Add employees test to department class

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -14,4 +14,15 @@ RSpec.describe Department do
     expect(customer_service.name).to eq("Customer Service")
     expect(customer_service.employees).to eq([])
   end
+
+  it 'can hire employees' do
+    customer_service = Department.new("Customer Service")
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    aaron = Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"}) 
+
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron)    
+
+      expect(customer_service.employees).to match_array(bobbi, aaron)  
+  end
 end
