028cd544d778c7ce1c164e0eb3cc74bc9e50ea15
Amy Spears <amspears007@gmail.com>
2023-01-13 09:11:44 -0700

    Initial commit and first passing 'exists' test in Employee class

---
+++ lib/employee.rb
@@ -0,0 +1,11 @@
+class Employee
+  attr_reader :name,
+              :age,
+              :salary
+
+  def initialize(info)
+    @name = info[:name]
+    @age = info[:age]
+    @salary = info[:salary]
+  end
+end
