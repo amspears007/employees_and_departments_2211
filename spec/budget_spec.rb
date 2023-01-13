3d96b15b212ce70a3db950a0d443559d7460e2ca
Amy Spears <amspears007@gmail.com>
2023-01-13 10:45:34 -0700

    Add list departments test for Budget class

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -19,4 +19,27 @@ RSpec.describe Budget do
     expect(budget2021.deparments).to eq([])
     expect(budget2022.deparments).to eq([])
   end
+
+  it 'can add and list departments' do
+    budget2021 = Budget.new("2021")
+    budget2022 = Budget.new("2022")
+
+    customer_service = Department.new("Customer Service") 
+    human_resources = Department.new("Human Resources")
+    it = Department.new("IT")
+
+    budget2021.add_department(customer_service)
+    budget2021.add_department(human_resources)
+    budget2021.add_department(it)
+
+    expect(budget2021.add_department).to match_array([customer_service, human_resources. it])
+
+
+
+
+
+
+
+    
+  end
 end
