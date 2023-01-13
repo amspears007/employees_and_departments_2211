5ae9e569b19c6479be717dddbe121c41f80351f8
Amy Spears <amspears007@gmail.com>
2023-01-13 09:23:13 -0700

    Add receive raise test

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe Employee do
     expect(bobbi.age).to eq(30)
     expect(bobbi.salary).to eq(100000)
   end
+
+  it 'can receive a raise' do
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "$100000"})
+
+    expect(bobbi.give_raise(5000)).to eq(105000)
+
+  end
 end
