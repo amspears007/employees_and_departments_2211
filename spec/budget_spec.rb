da63dd80c1f474ca8f45e62545fef6f673bb54f5
Amy Spears <amspears007@gmail.com>
2023-01-13 10:27:43 -0700

    Add year test to Budget class

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -3,11 +3,12 @@ require './lib/department'
 require './lib/employee'
 
 RSpec.describe Budget do
-  it 'exists' do
+  it 'exists and has a year' do
     budget2021 = Budget.new("2021")
     budget2022 = Budget.new("2022")
 
     expect(budget2021).to be_a(Budget)
     expect(budget2022).to be_a(Budget)
+    expect(budget2021.year).to eq("2021")
   end
 end
