f01d1ad372b3143d4741cbffde313d1c25e7bd85
Amy Spears <amspears007@gmail.com>
2023-01-13 09:40:55 -0700

    Add first two it blocks of tests

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -4,5 +4,14 @@ require './lib/employee'
 RSpec.describe Department do
   it 'exists' do
     customer_service = Department.new("Customer Service") 
+
+    expect(customer_service).to be_a(Department)
+  end
+
+  it 'has a name and starts with no employees' do
+    customer_service = Department.new("Customer Service")
+
+    expect(customer_service.name).to eq("Customer Service")
+    expect(customer_service.employees).to eq([])
   end
 end
